90a6d1ba6e43fafe7f9980f6bdfd02d9c6d3d197
marienshane01 <marienshane01@gmail.com>
2025-09-04 21:51:18 +0800

    First commit

---
+++ It20a.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.it20a;
+
+/**
+ *
+ * @author admin
+ */
+public class It20a {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
